681f90cf84129762f529230f9a3be81c4664a93e
Otarossoni <otarossoni@gmail.com>
2022-12-27 20:34:48 -0300

    feat: depósito e saque

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -11,7 +11,7 @@ const customers = [];
 
 // Middleware
 function verifyIfExistsAccountCPF(request, response, next) {
-  const { cpf } = request.params;
+  const { cpf } = request.headers;
 
   const customer = customers.find((customer) => customer.cpf == cpf);
 
@@ -19,11 +19,22 @@ function verifyIfExistsAccountCPF(request, response, next) {
     return response.status(400).json({ error: "Customer not found!" });
   }
 
-  console.log(customer);
-
   request.customer = customer;
 
   return next();
+}
+
+// Funções
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type == "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
 }
 
 app.post("/account", (request, response) => {
@@ -50,6 +61,44 @@ app.post("/account", (request, response) => {
 app.get("/statement/:cpf", verifyIfExistsAccountCPF, (request, response) => {
   const { customer } = request;
   return response.status(200).json(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (request, response) => {
+  const { description, amount } = request.body;
+
+  const { customer } = request;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
+  const { amount } = request.body;
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    return response.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return response.status(201).send();
 });
 
 // Porta na qual a aplicação estará rodando
