0f2e0c1c3b821de1554c6f0d76e7152b04b2769f
Otarossoni <otarossoni@gmail.com>
2023-01-14 21:20:41 -0300

    feat: persistência de dados de usuário em arquivo via API nativa

--- nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,26 +1,35 @@
 import http from "node:http";
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
+import { json } from "./middlewares/json.js";
 
-const users = [];
+const database = new Database();
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
+  await json(req, res);
+
   if (method === "GET" && url === "/users") {
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
+    const users = database.select("users");
+
+    return res.end(JSON.stringify(users));
   }
 
   if (method === "POST" && url === "/users") {
-    users.push({
-      id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
-    });
-    return res.end("Criação de Usuário");
+    const { name, email } = req.body;
+
+    const user = {
+      id: randomUUID(),
+      name,
+      email,
+    };
+    database.insert("users", user);
+
+    return res.writeHead(201).end();
   }
 
-  return res.end("Hello World!");
+  return res.writeHead(404).end();
 });
 
 server.listen(3333);
